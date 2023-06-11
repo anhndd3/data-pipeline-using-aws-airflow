73c93635b27f72ff89ef1633a032a5121e2b6359
anhndd3 <anhndd3@gmail.com>
2023-06-11 21:09:07 +0700

    Add the SQL file to set up the internal database.

---
+++ setup_database/sql/create_database.sql
@@ -0,0 +1,24 @@
+USE DATABASE database
+
+CREATE SCHEMA IF NOT EXISTS threats;
+
+DROP TABLE IF EXISTS threats.malware;
+
+CREATE TABLE threats.malware (
+    time_received TIMESTAMP,
+    download_source TEXT,
+    top_level_domain TEXT,
+    download_speed TEXT,
+    ping_time_to_server INT,
+    file_size_bytes NUMERIC,
+    how_many_times_file_seen INT,
+    executable_code_maybe_present_in_headers TEXT,
+    calls_to_low_level_system_libraries NUMERIC,
+    evidence_of_code_obfuscation TEXT,
+    threads_started INT,
+    mean_word_length_of_extracted_strings NUMERIC,
+    similarity_score NUMERIC,
+    characters_in_url INT,
+    actually_malicious TEXT,
+    initial_statistical_analysis TEXT
+);
